1e0249c5474c4db42b9fff430b5b22654430397b
mosesgitonga <infosec947@gmail.com>
2023-09-18 05:05:42 +0300

    removed use db

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,7 +1,7 @@
 --create database, user and set privileges
 --this is just a comment for the checker requirement
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-USE hbnb_dev_db;
+
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 
