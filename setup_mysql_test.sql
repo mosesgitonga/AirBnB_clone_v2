c8c5e7cf310de3d78d93d7b91a66025bd1e18e16
mosesgitonga <infosec947@gmail.com>
2023-09-18 06:14:59 +0300

    preparint myql server

---
+++ setup_mysql_test.sql
@@ -0,0 +1,12 @@
+-- a script that prepares mysql server
+-- create database
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+-- database to use
+USE hbnb_test_db;
+-- create user 
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY '1Ab@23456';
+-- set priviliges
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+-- grant select privlige
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;
