7376609a0df7e9a1982e27117f12d201b276c20b
mosesgitonga <infosec947@gmail.com>
2023-09-18 05:31:32 +0300

    proper commenting on sql

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,12 +1,13 @@
---create database, user and set privileges
---prepares mysql server for the project
+-- create database, user and set privileges
+-- prepares mysql server for the project
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
---use database
+-- use database
 USE hbnb_dev_db;
---creates db if it doen't exist
+-- creates db if it doen't exist
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
---grants privileges to hbnb_dev
+-- grants privileges to hbnb_dev
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
---grants select prievilidges to perfomance schema
+-- grants select prievilidges to perfomance schema
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
