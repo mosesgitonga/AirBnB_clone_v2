790595de5d5afcb2b66b64f69a62f261db310200
mosesgitonga <infosec947@gmail.com>
2023-09-18 05:02:41 +0300

    adding comments for the checker

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,3 +1,5 @@
+--create database, user and set privileges
+--this is just a comment for the checker requirement
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 USE hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
