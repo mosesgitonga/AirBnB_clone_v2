743119323e1fb0aa0ed38515dcd9442d41b1e462
mosesgitonga <infosec947@gmail.com>
2023-09-18 05:15:49 +0300

    added comments

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,8 +1,12 @@
 --create database, user and set privileges
---this is just a comment for the checker requirement
+--prepares mysql server for the project
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+--use database
 USE hbnb_dev_db;
+--creates db if it doen't exist
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+--grants privileges to hbnb_dev
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
+--grants select prievilidges to perfomance schema
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
