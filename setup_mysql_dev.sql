e4a166bf54b268e5b0a5eb77e5551430b4ffb911
mosesgitonga <infosec947@gmail.com>
2023-09-19 05:28:56 -0700

    changes

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,12 +1,12 @@
---create database, user and set privileges
---prepares mysql server for the project
+-- this script prepares a MySQL server for the project
+-- create project developement database with the name : hbnb_dev_db
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
---use database
-USE hbnb_dev_db;
---creates db if it doen't exist
+-- creating new user named : hbnb_dev with all privileges on the db hbnb_dev_db
+-- with the password : hbnb_dev_pwd if it dosen't exist
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
---grants privileges to hbnb_dev
+-- granting all privileges to the new user
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
---grants select prievilidges to perfomance schema
+-- granting the SELECT privilege for the user hbnb_dev in the db performance_schema
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
