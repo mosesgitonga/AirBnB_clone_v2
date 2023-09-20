3e4fb5ff12f0c76b634ad9a08c732bd90e06f39e
Judhunja <judxhunja@gmail.com>
2023-09-20 13:05:03 +0300

    fixing some syntax errors

--- models/city.py
+++ models/city.py
@@ -10,7 +10,7 @@ class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     __tablename__ = 'cities'
     if getenv('HBNB_TYPE_STORAGE') == 'db':
-        id = Column(String(60), primary_key= True)
+        id = Column(String(60), primary_key=True)
         name = Column(String(128), nullable=False)
         state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
         state = relationship('State', back_populates='city')
