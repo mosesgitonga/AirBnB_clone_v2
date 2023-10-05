300359a7fd4ce4ba705e8e5d783d64d21fdb86ac
mosesgitonga <infosec947@gmail.com>
2023-10-05 16:27:13 +0300

    syntax errors

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -2,11 +2,11 @@
 #a Fabric script that generates a .tgz archive from the contents of the
 # web_static folder of your AirBnB Clone repo, using the function do_pack
 
-from fabric,api import Connection, task, local
+from fabric.api import local
 #from collections.abc import Mapping
 from datetime import datetime
 
-@task
+
 def do_pack():
     """do_pack func to pack files in web_static"""
 
