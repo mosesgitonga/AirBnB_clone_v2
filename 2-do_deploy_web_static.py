5a42945290e1070be19c8d15908c433b8fc7eabc
mosesgitonga <infosec947@gmail.com>
2023-10-05 20:39:11 +0300

    deploying

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,49 +1,50 @@
 #!/usr/bin/python3
-"""a Fabric script (based on the file 1-pack_web_static.py) that 
-distributes an archive to your web servers, using the function do_deploy"""
+""" module doc
+"""
+from fabric.api import task, local, env, put, run
+from datetime import datetime
+import os
 
-import os.path
-from fabric.api import env
-from fabric.api import put
-from fabric.api import run
-env.hosts = ["54.175.146.62", "34.224.94.116"]
+env.hosts = ['18.207.1.87', '52.206.189.175']
 
 
+@task
+def do_pack():
+    """ method doc
+        sudo fab -f 1-pack_web_static.py do_pack
+    """
+    formatted_dt = datetime.now().strftime('%Y%m%d%H%M%S')
+    mkdir = "mkdir -p versions"
+    path = "versions/web_static_{}.tgz".format(formatted_dt)
+    print("Packing web_static to {}".format(path))
+    if local("{} && tar -cvzf {} web_static".format(mkdir, path)).succeeded:
+        return path
+    return None
+
+
+@task
 def do_deploy(archive_path):
+    """ method doc
+        fab -f 2-do_deploy_web_static.py do_deploy:
+        archive_path=versions/web_static_20231004201306.tgz
+        -i ~/.ssh/id_rsa -u ubuntu
     """
-    Distributing archive to the web server.
-    """
-    if os.path.isfile(archive_path) is False:
+    try:
+        if not os.path.exists(archive_path):
+            return False
+        fn_with_ext = os.path.basename(archive_path)
+        fn_no_ext, ext = os.path.splitext(fn_with_ext)
+        dpath = "/data/web_static/releases/"
+        put(archive_path, "/tmp/")
+        run("rm -rf {}{}/".format(dpath, fn_no_ext))
+        run("mkdir -p {}{}/".format(dpath, fn_no_ext))
+        run("tar -xzf /tmp/{} -C {}{}/".format(fn_with_ext, dpath, fn_no_ext))
+        run("rm /tmp/{}".format(fn_with_ext))
+        run("mv {0}{1}/web_static/* {0}{1}/".format(dpath, fn_no_ext))
+        run("rm -rf {}{}/web_static".format(dpath, fn_no_ext))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {}{}/ /data/web_static/current".format(dpath, fn_no_ext))
+        print("New version deployed!")
+        return True
+    except Exception:
         return False
-
-    archive = archive_path.split("/")[-1]
-    extensionLess = archive.split(".")[0]
-
-    if put(archive_path, "/tmp/{}".format(archive)).failed is True:
-        return False
-
-    if run("mkdir /data/web_static/releases/{}/".
-           format(extensionLess)).failed is True:
-        return False
-
-    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
-           format(archive, extensionLess)).failed is True:
-        return False
-
-    if run("rm /tmp/{}".format(archive)).failed is True:
-        return False
-
-    if run("rm -rf /data/web_static/current/").failed is True:
-        return False
-
-    if run("mkdir /data/web_static/current/").failed is True:
-        return False
-
-    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current/"
-           .format(extensionLess)).failed is True:
-        return False
-
-    if run("rm -rf /data/web_static/releases/{}/".
-           format(extensionLess)).failed is True:
-        return False
-    return True
