32702e00d31406c73c0dec8f11cb6ed7cc06b52b
root <infosec947@gmail.com>
2023-10-06 07:24:12 +0000

    deplyedserver 1

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -27,9 +27,10 @@ def do_pack():
         return None
 
 
+    return False
 def do_deploy(archive_path):
     """
-        Distribute archive.
+    Distribute archive.
     """
     if os.path.exists(archive_path):
         archived_file = archive_path[9:]
@@ -37,11 +38,14 @@ def do_deploy(archive_path):
         archived_file = "/tmp/" + archived_file
         put(archive_path, "/tmp/")
         run("sudo mkdir -p {}".format(newest_version))
-        run("sudo tar -xzf {} -C {}/".format(archived_file,
-                                             newest_version))
+        run("sudo tar -xzf {} -C {}/".format(archived_file, newest_version))
         run("sudo rm {}".format(archived_file))
-        run("sudo mv {}/web_static/* {}".format(newest_version,
-                                                newest_version))
+
+        # Remove existing directories if they exist
+        run("sudo rm -rf {}/web_static/images".format(newest_version))
+        run("sudo rm -rf {}/web_static/styles".format(newest_version))
+
+        run("sudo mv {}/web_static/* {}".format(newest_version, newest_version))
         run("sudo rm -rf {}/web_static".format(newest_version))
         run("sudo rm -rf /data/web_static/current")
         run("sudo ln -s {} /data/web_static/current".format(newest_version))
@@ -50,3 +54,4 @@ def do_deploy(archive_path):
         return True
 
     return False
+
