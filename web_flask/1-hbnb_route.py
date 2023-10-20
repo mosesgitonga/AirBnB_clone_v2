ef7992be08c5cf546f94eb2930cba9acbaa98d9e
mosesgitonga <infosec947@gmail.com>
2023-10-20 08:04:13 +0300

    2 routes

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,11 +1,14 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 from flask import Flask
-
 app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello():
     return 'Hello HBNB!'
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return 'HBNB'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
